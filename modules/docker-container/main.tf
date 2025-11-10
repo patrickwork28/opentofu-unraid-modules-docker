32fb5fc1c5f2735d0e09b24e792327a139e85d5b
Hoang Le <seein@nuc9ex.localdomain>
2025-11-10 22:55:38 +0700

    (dev) update module, ready to re-release version 1.0 2

---
+++ modules/docker-container/main.tf
@@ -0,0 +1,98 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6"
+    }
+  }
+}
+
+resource "docker_image" "this" {
+  name         = var.container_data.image
+  keep_locally = true
+}
+
+resource "docker_container" "this" {
+  name         = var.container_data.name
+  hostname     = var.container_data.hostname
+  image        = docker_image.this.image_id
+  restart      = var.container_data.restart
+  network_mode = var.container_data.network
+  cpu_set      = var.container_data.cpuset != "" ? var.container_data.cpuset : null
+
+  user       = var.container_data.user
+  privileged = var.container_data.privileged
+  command    = length(var.container_data.command) > 0 ? var.container_data.command : null
+  entrypoint = length(var.container_data.entrypoint) > 0 ? var.container_data.entrypoint : null
+
+  env = [
+    for env in var.container_data.envs : "${env.name}=${env.value}"
+  ]
+
+  dynamic "capabilities" {
+    for_each = var.container_data.capabilities != null && (length(try(var.container_data.capabilities.add, [])) > 0 || length(try(var.container_data.capabilities.drop, [])) > 0) ? [var.container_data.capabilities] : []
+    content {
+      add  = capabilities.value.add
+      drop = capabilities.value.drop
+    }
+  }
+
+  security_opts = var.container_data.security_opts
+
+  dynamic "ports" {
+    for_each = var.container_data.ports
+    content {
+      internal = ports.value.container
+      external = ports.value.host
+      protocol = ports.value.protocol
+    }
+  }
+
+  dynamic "mounts" {
+    for_each = var.container_data.mounts
+    content {
+      target    = mounts.value.container_path
+      source    = mounts.value.host_path
+      type      = mounts.value.type
+      read_only = mounts.value.mode == "ro"
+
+      dynamic "tmpfs_options" {
+        for_each = mounts.value.type == "tmpfs" && mounts.value.tmpfs_options != null ? [mounts.value.tmpfs_options] : []
+        content {
+          mode       = tmpfs_options.value.mode
+          size_bytes = tmpfs_options.value.size_bytes
+        }
+      }
+    }
+  }
+
+  dynamic "labels" {
+    for_each = var.container_data.labels
+    content {
+      label = labels.value.name
+      value = labels.value.value
+    }
+  }
+
+  dynamic "devices" {
+    for_each = var.container_data.devices
+    content {
+      host_path      = devices.value.host_path
+      container_path = devices.value.container_path
+      permissions    = devices.value.permissions
+    }
+  }
+
+  dynamic "upload" {
+    for_each = var.container_data.configs
+    content {
+      file           = upload.value.file
+      content        = upload.value.content
+      content_base64 = upload.value.content_base64
+      source         = upload.value.source
+      source_hash    = upload.value.source != null ? filemd5(upload.value.source) : upload.value.source_hash
+      executable     = upload.value.executable
+      permissions    = upload.value.permissions
+    }
+  }
+}
---
+++ modules/unraid-docker-xml/main.tf
@@ -0,0 +1,118 @@
+locals {
+  # Transform shared and XML-specific data into template format
+  template_data = {
+    container_name = var.shared_container_data.name
+    repository     = var.shared_container_data.image
+    registry       = try(var.xml_specific_data.registry, "")
+    network        = var.shared_container_data.network
+    shell          = var.xml_specific_data.shell
+    privileged     = var.shared_container_data.privileged ? "true" : "false"
+    support        = var.xml_specific_data.support
+    project        = var.xml_specific_data.project
+    overview       = var.xml_specific_data.overview
+    category       = var.xml_specific_data.category
+    webui          = var.xml_specific_data.webui
+    template_url   = var.xml_specific_data.template_url
+    icon           = var.xml_specific_data.icon
+    extra_params   = var.xml_specific_data.extra_params
+    date_installed = var.xml_specific_data.date_installed
+    donate_text    = var.xml_specific_data.donate_text
+    donate_link    = var.xml_specific_data.donate_link
+    myip           = var.xml_specific_data.myip
+    postargs       = var.xml_specific_data.postargs
+    cpuset         = var.xml_specific_data.cpuset
+    requires       = var.xml_specific_data.requires
+
+    # Generate configs from shared data following Unraid UI order
+    configs = concat(
+      # Environment variables (Type: Variable)
+      [
+        for env in try(var.shared_container_data.envs, []) : {
+          name        = env.name
+          target      = env.name
+          default     = ""
+          mode        = ""
+          description = ""
+          type        = "Variable"
+          display     = "always"
+          required    = "false"
+          mask        = "false"
+          value       = env.value
+        }
+      ],
+      # Port mappings (Type: Port)
+      [
+        for port in try(var.shared_container_data.ports, []) : {
+          name        = "Port ${port.container}"
+          target      = "${port.container}"
+          default     = "${port.host}"
+          mode        = "${port.protocol}"
+          description = ""
+          type        = "Port"
+          display     = "always"
+          required    = "false"
+          mask        = "false"
+          value       = "${port.host}"
+        }
+      ],
+      # Volume mounts (Type: Path)
+      [
+        for mount in try(var.shared_container_data.mounts, []) : {
+          name        = "Path ${mount.container_path}"
+          target      = mount.container_path
+          default     = mount.host_path
+          mode        = mount.mode
+          description = ""
+          type        = "Path"
+          display     = "always"
+          required    = "false"
+          mask        = "false"
+          value       = mount.host_path
+        }
+      ],
+      # Device mappings (Type: Device)
+      [
+        for device in try(var.shared_container_data.devices, []) : {
+          name        = "Device ${device.container_path}"
+          target      = device.container_path
+          default     = device.host_path
+          mode        = device.permissions
+          description = ""
+          type        = "Device"
+          display     = "always"
+          required    = "false"
+          mask        = "false"
+          value       = device.host_path
+        }
+      ],
+      # Labels (Type: Label)
+      [
+        for label in try(var.shared_container_data.labels, []) : {
+          name        = "Label ${label.name}"
+          target      = label.name
+          default     = ""
+          mode        = ""
+          description = ""
+          type        = "Label"
+          display     = "always"
+          required    = "false"
+          mask        = "false"
+          value       = label.value
+        }
+      ]
+    )
+  }
+}
+
+resource "null_resource" "docker_unraid_xml" {
+  count = var.enable == true ? 1 : 0
+  triggers = {
+    content = templatefile("${path.module}/template.xml.tpl", local.template_data)
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo '${templatefile("${path.module}/template.xml.tpl", local.template_data)}' | ssh -i ${var.unraid_ssh_privatekey} -o StrictHostKeyChecking=no ${var.unraid_user}@${var.unraid_host} "cat > ${var.unraid_xml_folderpath}/my-${var.shared_container_data.name}.xml"
+    EOT
+  }
+}
